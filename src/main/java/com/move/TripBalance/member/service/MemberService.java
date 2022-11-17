9cb131424efd5d96bd6cbdb7b9f6ba7950bc0656
DESKTOP-PE8J3UP\주은 <4708408@naver.com>
2022-11-18 01:38:02 +0900

    TB-43 내 정보 수정하기 및 프로필 사진 등록하기, 마이페이지 페이징 처리

--- src/main/java/com/move/TripBalance/member/service/MemberService.java
+++ src/main/java/com/move/TripBalance/member/service/MemberService.java
@@ -26,31 +26,31 @@ import java.util.Optional;
 @Service
 public class MemberService {
 
-  private final MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 
     private final PasswordEncoder passwordEncoder;
     private final TokenProvider tokenProvider;
     private final SNSRepository snsRepository;
 
-  //회원가입
-  @Transactional
-  public ResponseEntity<PrivateResponseBody> createMember(MemberRequestDto requestDto) {
-    // 아이디 중복 확인
-    if (null != isPresentMember(requestDto.getEmail())) {
-      return new ResponseEntity<>(new PrivateResponseBody
-              (StatusCode.DUPLICATED_NICKNAME,null),HttpStatus.OK);
-    }
+    //회원가입
+    @Transactional
+    public ResponseEntity<PrivateResponseBody> createMember(MemberRequestDto requestDto) {
+        // 아이디 중복 확인
+        if (null != isPresentMember(requestDto.getEmail())) {
+            return new ResponseEntity<>(new PrivateResponseBody
+                    (StatusCode.DUPLICATED_NICKNAME, null), HttpStatus.OK);
+        }
 
-    // 비밀번호 중복 확인
-    if (!requestDto.getPw().equals(requestDto.getPwConfirm())) {
-      return new ResponseEntity<>(new PrivateResponseBody
-              (StatusCode.DUPLICATED_PASSWORD,null),HttpStatus.OK);
-    }
+        // 비밀번호 중복 확인
+        if (!requestDto.getPw().equals(requestDto.getPwConfirm())) {
+            return new ResponseEntity<>(new PrivateResponseBody
+                    (StatusCode.DUPLICATED_PASSWORD, null), HttpStatus.OK);
+        }
 
-    // 회원 정보 저장
-    Member member = Member.builder()
-            .email(requestDto.getEmail())
-            .nickName(requestDto.getNickName())
+        // 회원 정보 저장
+        Member member = Member.builder()
+                .email(requestDto.getEmail())
+                .nickName(requestDto.getNickName())
                 .pw(passwordEncoder.encode(requestDto.getPw()))
                 .profileImg("기본사진 입니다.")
                 .self("자기소개를 등록해주세요")
@@ -64,75 +64,75 @@ public class MemberService {
                 .member(member)
                 .build());
 
-    // Message 및 Status를 Return
-    return new ResponseEntity<>(new PrivateResponseBody
-            (StatusCode.OK,"회원가입 성공"),HttpStatus.OK);
-  }
-
-  //로그인
-  @Transactional
-  public ResponseEntity<PrivateResponseBody> login(LoginRequestDto requestDto, HttpServletResponse response) {
-    Member member = isPresentMember(requestDto.getEmail());
-
-    // DB에 Email 확인
-    if (null == member) {
-      return new ResponseEntity<>(new PrivateResponseBody
-              (StatusCode.LOGIN_MEMBER_ID_FAIL,null),HttpStatus.OK);
+        // Message 및 Status를 Return
+        return new ResponseEntity<>(new PrivateResponseBody
+                (StatusCode.OK, "회원가입 성공"), HttpStatus.OK);
     }
 
-    // DB에 PW 확인
-    if (!member.validatePassword(passwordEncoder, requestDto.getPw())) {
-      return new ResponseEntity<>(new PrivateResponseBody
-              (StatusCode.LOGIN_PASSWORD_FAIL,null),HttpStatus.OK);
+    //로그인
+    @Transactional
+    public ResponseEntity<PrivateResponseBody> login(LoginRequestDto requestDto, HttpServletResponse response) {
+        Member member = isPresentMember(requestDto.getEmail());
+
+        // DB에 Email 확인
+        if (null == member) {
+            return new ResponseEntity<>(new PrivateResponseBody
+                    (StatusCode.LOGIN_MEMBER_ID_FAIL, null), HttpStatus.OK);
+        }
+
+        // DB에 PW 확인
+        if (!member.validatePassword(passwordEncoder, requestDto.getPw())) {
+            return new ResponseEntity<>(new PrivateResponseBody
+                    (StatusCode.LOGIN_PASSWORD_FAIL, null), HttpStatus.OK);
+        }
+
+        //토큰 지급
+        TokenDto tokenDto = tokenProvider.generateTokenDto(member);
+        tokenToHeaders(tokenDto, response);
+
+        // Message 및 Status를 Return
+        return new ResponseEntity<>(new PrivateResponseBody
+                (StatusCode.OK, MemberResponseDto.builder()
+                        .email(member.getEmail())
+                        .nickName(member.getNickName())
+                        .build()), HttpStatus.OK);
+    }
+
+    //로그아웃
+    public ResponseEntity<PrivateResponseBody> logout(HttpServletRequest request) {
+
+        // 토큰 확인
+        if (!tokenProvider.validateToken(request.getHeader("Refresh-Token"))) {
+            return new ResponseEntity<>(new PrivateResponseBody
+                    (StatusCode.LOGIN_WRONG_FORM_JWT_TOKEN, null), HttpStatus.OK);
+        }
+        Member member = tokenProvider.getMemberFromAuthentication();
+
+        // 회원 확인
+        if (null == member) {
+            return new ResponseEntity<>(new PrivateResponseBody
+                    (StatusCode.LOGIN_MEMBER_ID_FAIL, null), HttpStatus.OK);
+        }
+
+        tokenProvider.deleteRefreshToken(member);
+
+        // Message 및 Status를 Return
+        return new ResponseEntity<>(new PrivateResponseBody
+                (StatusCode.OK, "로그아웃"), HttpStatus.OK);
+    }
+
+    //Email 확인
+    @Transactional(readOnly = true)
+    public Member isPresentMember(String email) {
+        Optional<Member> optionalMember = memberRepository.findByEmail(email);
+        return optionalMember.orElse(null);
     }
 
     //토큰 지급
-    TokenDto tokenDto = tokenProvider.generateTokenDto(member);
-    tokenToHeaders(tokenDto, response);
-
-    // Message 및 Status를 Return
-    return new ResponseEntity<>(new PrivateResponseBody
-            (StatusCode.OK, MemberResponseDto.builder()
-                    .email(member.getEmail())
-                    .nickName(member.getNickName())
-                    .build()),HttpStatus.OK);
-  }
-
-  //로그아웃
-  public ResponseEntity<PrivateResponseBody> logout(HttpServletRequest request) {
-
-    // 토큰 확인
-    if (!tokenProvider.validateToken(request.getHeader("Refresh-Token"))) {
-      return new ResponseEntity<>(new PrivateResponseBody
-              (StatusCode.LOGIN_WRONG_FORM_JWT_TOKEN,null),HttpStatus.OK);
+    public void tokenToHeaders(TokenDto tokenDto, HttpServletResponse response) {
+        response.addHeader("Authorization", "Bearer " + tokenDto.getAccessToken());
+        response.addHeader("Refresh-Token", tokenDto.getRefreshToken());
+        response.addHeader("Access-Token-Expire-Time", tokenDto.getAccessTokenExpiresIn().toString());
     }
-    Member member = tokenProvider.getMemberFromAuthentication();
-
-    // 회원 확인
-    if (null == member) {
-      return new ResponseEntity<>(new PrivateResponseBody
-              (StatusCode.LOGIN_MEMBER_ID_FAIL,null),HttpStatus.OK);
-    }
-
-    tokenProvider.deleteRefreshToken(member);
-
-    // Message 및 Status를 Return
-    return new ResponseEntity<>(new PrivateResponseBody
-            (StatusCode.OK,"로그아웃"),HttpStatus.OK);
-  }
-
-  //Email 확인
-  @Transactional(readOnly = true)
-  public Member isPresentMember(String email) {
-    Optional<Member> optionalMember = memberRepository.findByEmail(email);
-    return optionalMember.orElse(null);
-  }
-
-  //토큰 지급
-  public void tokenToHeaders(TokenDto tokenDto, HttpServletResponse response) {
-    response.addHeader("Authorization", "Bearer " + tokenDto.getAccessToken());
-    response.addHeader("Refresh-Token", tokenDto.getRefreshToken());
-    response.addHeader("Access-Token-Expire-Time", tokenDto.getAccessTokenExpiresIn().toString());
-  }
 
 }
