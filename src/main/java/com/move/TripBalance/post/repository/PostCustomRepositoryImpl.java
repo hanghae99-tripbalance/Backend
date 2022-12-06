5a0cdffc0474b0eac9ae9e2e29ce7618e5c17db0
dydrkflqmdl <dydrkflqmdl@naver.com>
2022-12-06 19:02:57 +0900

    TB-80 Fix 전체 게시글 조회 QueryDSL 코드 진행

---
+++ src/main/java/com/move/TripBalance/post/repository/PostCustomRepositoryImpl.java
@@ -0,0 +1,55 @@
+package com.move.TripBalance.post.repository;
+
+import com.move.TripBalance.heart.Heart;
+import com.move.TripBalance.member.Member;
+import com.move.TripBalance.post.Post;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import static com.move.TripBalance.post.QPost.post;
+
+@Repository
+@RequiredArgsConstructor
+public class PostCustomRepositoryImpl implements PostCustomRepository {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public Page<Post> findAllByOrderByCreatedAtDesc(Pageable pageable) {
+
+        Long count = jpaQueryFactory
+                .select(post.count())
+                .from(post)
+                .fetchOne();
+
+        List<Post> postList =jpaQueryFactory
+                .selectFrom(post)
+                .orderBy(post.createdAt.desc())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        return new PageImpl<>(postList, pageable, count);
+    }
+
+//    @Override
+//    public Page<Post> search(String keyword, Pageable pageable) {
+//        return null;
+//    }
+//
+//    @Override
+//    public List<Post> findAllByMember(Member member) {
+//        return null;
+//    }
+//
+//    @Override
+//    public List<Post> findTop10ByHeartsIn(List<Heart> hearts) {
+//        return null;
+//    }
+}
--- src/main/java/com/move/TripBalance/post/repository/PostRepository.java
+++ src/main/java/com/move/TripBalance/post/repository/PostRepository.java
@@ -11,9 +11,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
 
 //  Page<Post> findAllByOrderByCreatedAtDesc(Pageable pageable);
